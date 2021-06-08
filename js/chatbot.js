5eeb82987fa73318fad164a3d419674da6e5e275
beso <besogigashvili818@gmail.com>
2021-06-08 22:38:31 +0400

    details

--- js/chatbot.js
+++ js/chatbot.js
@@ -1,12 +1,16 @@
-const messengerBtn = document.getElementById("messenger-button");
-const closeBtn = document.getElementById("close-chatbox")
-const content = document.getElementById("messenger-content");
-// close/open messenger content 
-messengerBtn.addEventListener("click",function(){
-    content.style.display = "block";
-    messengerBtn.style.display = "none";
-});
-closeBtn.addEventListener("click",function(){
-    content.style.display = "none";
-    messengerBtn.style.display = "flex";
-});
+
+
+
+
+// const messengerBtn = document.getElementById("messenger-button");
+// const closeBtn = document.getElementById("close-chatbox")
+// const content = document.getElementById("messenger-content");
+// // close/open messenger content 
+// messengerBtn.addEventListener("click",function(){
+//     content.style.display = "block";
+//     messengerBtn.style.display = "none";
+// });
+// closeBtn.addEventListener("click",function(){
+//     content.style.display = "none";
+//     messengerBtn.style.display = "flex";
+// });
